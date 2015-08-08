17dc50800c5ab556b5dc46478b6cda9e44888372
junshiguo <junshiguo@126.com>
2015-08-08 13:25:49 +0800

    add random id hash function, using 64 bit FNV hash

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,48 +4,55 @@ import impl.RequestAspectImpl;
 import impl.UserAspectImpl;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.hibernate.Session;
 
+import util.RandomUtil;
 import domain.Rating;
-import domain.RequestActive;
 import domain.User;
 
 public class Test {
 	
     
     public static void main(String[] args) throws IOException {
-//    	Session session = MySessionFactory.getSessionFactory().getCurrentSession();
-//		session.beginTransaction();
-//		RequestActive request = (RequestActive) session.get(RequestActive.class, "444");
-//		String sourcename = request.getSourceName();
-//		System.out.println(sourcename);
-//		System.out.println(request.getDestinationName());
-    	
+    	ArrayList<Long> hashval = new ArrayList<Long>();
+    	for(long i = 0; i < 1000000L; i++){
+    		long hashed = RandomUtil.FNVhash64(i);
+    		if(hashval.contains(new Long(hashed))){
+    			System.out.println("repeated hashed val for "+i);
+    		}else{
+    			hashval.add(hashed);
+    		}
+    	}
+    		
+    }
+    
+    public static void testAddRequest(){
     	RequestAspectImpl request = new RequestAspectImpl();
     	System.out.println(request.addRequest("18801735863", 1.0, 1.2, "复旦大学", 2.0, 2.2, "张江", "2015-8-6 10:10:10", 20, 30, 0));
+        MySessionFactory.getSessionFactory().close();
+    }
+    
+    public static void testUser(){
+    	UserAspectImpl test = new UserAspectImpl();
+    	System.out.println(test.sendVerifyCode("18801735863").toString());
+    	String result = test.register("18801735863", "123456", "jsguo", User.GENDER_MALE, null, 24);
+    	System.out.println(result.toString());
     	
-//        Test mgr = new Test();
-//        mgr.createAndStoreOrder(1, new Date());
-//    	UserAspectImpl test = new UserAspectImpl();
-//    	System.out.println(test.sendVerifyCode("18801735863").toString());
-//    	String result = test.register("18801735863", "123456", "jsguo", User.GENDER_MALE, null, 24);
-//    	System.out.println(result.toString());
-//    	
-//    	result = test.logIn("18801735863", "123456");
-//    	System.out.println(result.toString());
-//    	
-//    	result = test.changePassword("18801735863", "1234", "123456");
-//    	System.out.println(result.toString());
-//    	
-//    	result = test.findPassword("1880173863");
-//    	System.out.println(result.toString());
+    	result = test.logIn("18801735863", "123456");
+    	System.out.println(result.toString());
+    	
+    	result = test.changePassword("18801735863", "1234", "123456");
+    	System.out.println(result.toString());
+    	
+    	result = test.findPassword("1880173863");
+    	System.out.println(result.toString());
         MySessionFactory.getSessionFactory().close();
     }
 
-    private void createAndStoreOrder(int id, Date theDate) {
+    public static void createAndStoreOrder(int id, Date theDate) {
         Session session = MySessionFactory.getSessionFactory().getCurrentSession();
         session.beginTransaction();
 
--- src/main/java/util/RandomUtil.java
+++ src/main/java/util/RandomUtil.java
@@ -29,4 +29,32 @@ public class RandomUtil {
 		return ""+(currentOrderId++);
 	}
 	
+	public static long hash(long val){
+		return FNVhash64(val);
+	}
+	
+	 public static final long FNV_offset_basis_64=0xCBF29CE484222325L;
+     public static final long FNV_prime_64=1099511628211L;
+     
+     /**
+      * 64 bit FNV hash. Produces more "random" hashes than (say) String.hashCode().
+      * 
+      * @param val The value to hash.
+      * @return The hash value
+      */
+	public static long FNVhash64(long val) {
+		// from http://en.wikipedia.org/wiki/Fowler_Noll_Vo_hash
+		long hashval = FNV_offset_basis_64;
+
+		for (int i = 0; i < 8; i++) {
+			long octet = val & 0x00ff;
+			val = val >> 8;
+
+			hashval = hashval ^ octet;
+			hashval = hashval * FNV_prime_64;
+			// hashval = hashval ^ octet;
+		}
+		return Math.abs(hashval);
+	}
+	
 }
